2577b78ec12a1af68d3a9f720f43a5e6803028f2
Uboon <ChrisJacobBoon@gmail.com>
2022-06-28 15:53:53 -0400

    Created table update scheme for cleaning blank rows and DATETIME error

--- TicketQuery.sql
+++ TicketQuery.sql
@@ -1,32 +1,70 @@
 
-Select [Date/Time]
-From MontgomeryTickets..TicketLocations$
+------(1)---------Selecting Useful Data (ignoring Blank rows)-----------
+Select *
+From ParkingTickets..TicketLocations$
+Where [Ticket Number] is not null
+and [Date/Time] is not null
+and [Ticket Location] is not null
 
-UPDATE MontgomeryTickets..TicketLocations$
+---TESTING---Before we update we make a copy of the table and check our UPDATE strategy
+
+DROP TABLE IF EXISTS #temp_locations
+SELECT * INTO #temp_locations
+FROM ParkingTickets..TicketLocations$
+--Where clause be used here, but our purpose is to check if the UPDATE works before implementing
+
+UPDATE #temp_locations
+SET [Ticket Number]=[Ticket Number],[Date/Time]=[Date/Time],[Ticket Location]=[Ticket Location]
+Where [Ticket Number] is not null
+and [Date/Time] is not null
+and [Ticket Location] is not null
+-- 102467 rows affected, Great! UPDATE works as we wanted!
+
+--Updating Our two Tables
+UPDATE ParkingTickets..TicketLocations$
+SET [Ticket Number]=[Ticket Number],[Date/Time]=[Date/Time],[Ticket Location]=[Ticket Location]
+Where [Ticket Number] is not null
+and [Date/Time] is not null
+and [Ticket Location] is not null
+
+UPDATE ParkingTickets..TicketViolations$
+SET [Ticket Number]=[Ticket Number],[Violation Code]=[Violation Code],
+[Violation Description]=[Violation Description]
+Where [Ticket Number] is not null
+and [Violation Code] is not null
+and [Violation Description] is not null
+---------------------------------------------Removal of Blank data complete--------------
+
+----(2)-------------Date Time Error---------------------------
+
+Select [Date/Time]
+From ParkingTickets..TicketLocations$
+
+UPDATE ParkingTickets..TicketLocations$
 SET [Date/Time] = Replace([Date/Time],'0020','2020')
 
-UPDATE MontgomeryTickets..TicketLocations$
+UPDATE ParkingTickets..TicketLocations$
 SET [Date/Time] = Replace([Date/Time],'0021','2021')
 
-Select [ Ticket ID] as ID, FORMAT(convert(DATETIME,[Date/Time]),'MM/dd/yyyy hh:mm tt') as [Date], 
-[Ticket Location] 
-From MontgomeryTickets..TicketLocations$
-Where [Date/Time] is not null and [Ticket Location] is not null
------------------------------------------------------------------
+UPDATE ParkingTickets..TicketLocations$
+SET [Date/Time] = FORMAT(convert(DATETIME,[Date/Time]),'MM/dd/yyyy hh:mm tt')
+----------------------DATE Error Fix Complete--------------------------------
+
+---(3)----Calculating Measures-------------------------------------------------------
 --Most frequent violation
 Select [Violation Code],[Violation Description], Count([Violation Code]) as vio_count
-From MontgomeryTickets..TicketViolations$
+From ParkingTickets..TicketViolations$
 Where [Violation Code] is not null 
 and [Violation Description] is not null
 GROUP by [Violation Code],[Violation Description]
 order by 3 desc
 
 -------------------------------------------------------------
-Select loc.[ Ticket ID],FORMAT(convert(DATETIME,loc.[Date/Time]),'MM/dd/yyyy hh:mm tt') as [Date],
+Select loc.[Ticket Number],FORMAT(convert(DATETIME,loc.[Date/Time]),'MM/dd/yyyy hh:mm tt') as [Date],
 loc.[Ticket Location], vio.[Violation Code],vio.[Violation Description]
-From MontgomeryTickets..TicketLocations$ as loc
-FULL OUTER JOIN MontgomeryTickets..TicketViolations$ as vio
-on loc.[ Ticket ID] = vio.[ Ticket ID]
+From ParkingTickets..TicketLocations$ as loc
+FULL OUTER JOIN ParkingTickets..TicketViolations$ as vio
+on loc.[Ticket Number] = vio.[Ticket Number]
 Where loc.[Date/Time] is not null 
 and loc.[Ticket Location] is not null 
 and vio.[Violation Code] is not null 
@@ -34,9 +72,9 @@ and vio.[Violation Description] is not null
 
 Select vio.[Violation Code],
 Cast(Count(vio.[Violation Code]) as float)/cast(Count(vio.[Violation Description]) as float) as rel_freq
-From MontgomeryTickets..TicketLocations$ as loc
-FULL OUTER JOIN MontgomeryTickets..TicketViolations$ as vio
-on loc.[ Ticket ID] = vio.[ Ticket ID]
+From ParkingTickets..TicketLocations$ as loc
+FULL OUTER JOIN ParkingTickets..TicketViolations$ as vio
+on loc.[Ticket Number] = vio.[Ticket Number]
 Where loc.[Date/Time] is not null 
 and loc.[Ticket Location] is not null 
 and vio.[Violation Code] is not null 
@@ -48,7 +86,7 @@ Order by 2 desc
 Select [Violation Description] , Cast(vioCount as numeric)/Cast(sum(vioCount) as numeric) as rel_freq
 FROM (
 	Select [Violation Description], COUNT([Violation Description]) as vioCount
-	FROM MontgomeryTickets..TicketViolations$
+	FROM ParkingTickets..TicketViolations$
 	Where [Violation Description] is not null
 	and [Violation Code] is not null
 	GROUP BY [Violation Description]
@@ -58,10 +96,10 @@ FROM (
 SELECT 
 FROM (
 	Select SUM(CAST(v2.vioCount as float))
-	From MontgomeryTickets..TicketViolations$ v1
+	From ParkingTickets..TicketViolations$ v1
 	LEFT JOIN ( 
 		SELECT [Violation Description], Count([Violation Description]) as vioCount
-		FROM MontgomeryTickets..TicketViolations$
+		FROM ParkingTickets..TicketViolations$
 		Where [Violation Description] is not null
 		and [Violation Code] is not null
 		GROUP BY [Violation Description]
@@ -73,13 +111,13 @@ FROM (
 DECLARE @vio_total decimal(16,2)
 SET @vio_total = (
 	SELECT cast(COUNT(*) as decimal(16,2))
-	FROM MontgomeryTickets..TicketViolations$
+	FROM ParkingTickets..TicketViolations$
 	WHERE [Violation Description] is not null
 	and [Violation Code] is not null)
 
 SELECT [Violation Description], Count([Violation Description]) as vioCount ,
 (CAST(Count([Violation Description]) as decimal(16,2))/@vio_total)*100 as rel_freq
-FROM MontgomeryTickets..TicketViolations$
+FROM ParkingTickets..TicketViolations$
 Where [Violation Description] is not null
 and [Violation Code] is not null
 GROUP BY [Violation Description]
